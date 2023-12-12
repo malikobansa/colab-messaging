7d0577eb56e4026617851d400c899f9bbdfbd76c
malikobansa <hackobansa@gmail.com>
2023-12-12 15:36:51 +0100

    First commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Auth/Login.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Auth/Login.jsx'
+import SignUp from './components/Auth/SignUp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
       <Route path='/login' element={<Login/>}/>
-      <Route path='/' element={<App/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
     </Routes>
   </BrowserRouter>
 )
